07a7ce9f40ecbe46bd9f6db9bdc490353cbc7063
ericlin1230 <erichungyu.lin@mail.utoronto.ca>
2021-09-17 22:56:10 -0400

    Finished image post request

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,40 @@
 var express = require('express');
 var router = express.Router();
 const multer = require('multer');
+var upload = multer({ dest: 'storage/' })
+const { path } = require('../app');
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
+// var storage = multer.diskStorage({
+//   destination: './storage/',
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+//   }
+// })
 
+// const upload = multer({
+//   storage: storage
+// }).single();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/upload', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.post('/upload', function (req, res) {
+//   upload(req, res, (err) => {
+//     if (err) {
+//       res.render('index', {
+//         msg: err
+//       });
+//     } else {
+//       console.log(req.file);
+//       res.send('test');
+//     }
+//   });
+// });
+
+router.post('/ay', upload.array('file', 12), function (req, res, next) {
+  console.log(req.files)
+  res.send("done");
 });
 
 module.exports = router;
