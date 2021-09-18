8d7b712ec4ffcf8628ef7e29050ee4dc0f1748f7
ericlin1230 <erichungyu.lin@mail.utoronto.ca>
2021-09-17 22:32:35 -0400

    Updated gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/upload', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
