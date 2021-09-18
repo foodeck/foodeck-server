28427bcdd08cb356cc8860f15bdb9b72446057cb
ericlin1230 <erichungyu.lin@mail.utoronto.ca>
2021-09-18 11:42:04 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const multer = require('multer');
 const vision = require('@google-cloud/vision');
+const imageProcess = require('../helpers/imageProcess.js');
 
 const client = new vision.ImageAnnotatorClient({
   keyFilename: './apik.json'
@@ -15,7 +16,6 @@ router.get('/', function (req, res, next) {
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 router.post('/upload', upload.any(), async function (req, res, next) {
-  const imageProcess = require('../helpers/imageProcess.js');
   var return_res = await imageProcess.imageToLabels(req,client);
   res.send(return_res)
 });
