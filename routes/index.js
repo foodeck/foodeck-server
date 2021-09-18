68815baaca0b27bb3730c88d5bdd834315fcd819
ericlin1230 <erichungyu.lin@mail.utoronto.ca>
2021-09-18 11:23:27 -0400

    Finished multiple image requests

--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,22 @@ router.get('/', function (req, res, next) {
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
-router.post('/upload', upload.single('file'), async function (req, res) {
-  const [result] = await client.labelDetection(req.file.buffer);
-  res.send(result);
+router.post('/upload', upload.any(), async function (req, res, next) {
+  console.log(req.files)
+  var final_res = [];
+  for (var i = 0; i<req.files.length; i++){
+    const [result] = await client.labelDetection(req.files[i].buffer);
+    var label_final = [];
+    for (var j=0; j<result.labelAnnotations.length; j++){
+      const labels = result.labelAnnotations[j].description;
+      const score = result.labelAnnotations[j].score;
+      const temp = {'description': labels, 'score': score}
+      label_final.push(temp);
+    }
+    final_res = final_res.concat(label_final);
+  }
+  var return_res = {"result": final_res}
+  res.send(return_res)
 });
 
 module.exports = router;
